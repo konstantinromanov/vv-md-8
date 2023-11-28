bb0898bbaa642944780a6d8cee0aae1be4a0d461
Konstantins Romanovs <konstantin.hvac@gmail.com>
2023-11-28 10:08:14 +0200

    Cleans the stuff

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //[Header("References")]
-    //public Rigidbody rb;
-    //public Transform head;
-    //public Camera camera;
-
-    //[Header("Configurations")]
-    //public float walkSpeed;
-    //public float runSpeed;
-
     public float sensX;
     public float sensY;
     public Transform orientation;
@@ -29,8 +20,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
-
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
@@ -42,36 +31,4 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
-
-    //private void FixedUpdate()
-    //{
-    //    //Vector3 newVelocity = Vector3.up * rb.velocity.y;
-    //    //float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-    //    //newVelocity.x = Input.GetAxis("Horizontal") * speed;
-    //    //newVelocity.z = Input.GetAxis("Vertical") * speed;
-    //    //rb.velocity = newVelocity;
-    //}
-
-    //private void LateUpdate()
-    //{
-    //    //Vector3 e = head.eulerAngles;
-    //    //e.x -= Input.GetAxis("Mouse Y") * 2f;
-    //    //e.x = Mathf.Clamp(e.x, -85f, 85f);
-    //    //head.eulerAngles = e;
-    //}
-
-    //private static float RestrictAngle(float angle, float angleMin, float angleMax)
-    //{
-    //    if (angle > angleMax)
-    //    {
-    //        return angleMax;
-    //    }
-
-    //    if(angle < angleMin)
-    //    {
-    //        return angleMin;
-    //    }
-
-    //    return angle;
-    //}
 }
